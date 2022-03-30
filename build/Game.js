495a02ff988a085c503a477c6b04e99198b986cb
Lucas Webb <lucasawebb@gmail.com>
2022-03-30 12:43:03 -0600

    Initial Project Setup

---
+++ build/Game.js
@@ -0,0 +1,5 @@
+function displayTotalPerPerson(person, total) {
+    var message = "Total for " + person + " is " + total;
+    document.getElementById("totalMessage").innerText = message;
+}
+//# sourceMappingURL=Game.js.map
